7a5c388fdb3b18dbdf937f6d946bbc333967d1a7
35V LG84 <35vlg84-x4e6b92@e257.fi>
2019-02-14 13:15:14 +0200

    Perf: support perf testing of git
    Add perf testing for git. Only input path is different, so
    only balance test is used.
    
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- perf/bin/perf-all.sh
+++ perf/bin/perf-all.sh
@@ -6,28 +6,56 @@ reports="balance balance-group register"
 #reports="balance"
 
 sets="1E3 1E4 1E5 1E6"
-#sets="1E6"
+#sets="1E5"
 
-#versions="0.4.1 0.5.0 0.6.0 0.7.0 0.8.0 0.9.0"
-versions="0.10.0"
+
+#versions="0.4.1 0.5.0 0.6.0 0.7.0 0.8.0 0.9.0 0.10.0"
+versions="0.23.0-SNAPSHOT"
+
+fltStr="base64:"$(cat << EOF | base64 --wrap=0
+{ "txnFilter": { "TxnFilterAND" : { "txnFilters" : [ { "TxnFilterTxnCode": { "regex": "#.*" }},  { "TxnFilterTxnDescription": { "regex": "txn-.*" } } ] } } }
+EOF
+)
+
 
 for v in $versions; do
 for s in $sets; do
 for r in $reports; do
-for frmt in txt json "txt json"; do
-for flt in "" '{ "txnFilter": { "TxnFilterAND" : { "txnFilters" : [ { "TxnFilterTxnCode": { "regex": "#.*" }},  { "TxnFilterTxnDescription": { "regex": "txn-.*" } } ] } } }'; do
 
-if [ -n "$flt" ]; then
-       log_flt="filter"
+for frmt in txt json "txt json"; do
+#for frmt in txt; do
+
+for filter in "" "$fltStr"; do
+#for filter in ""; do
+
+if [ -n "$filter" ]; then
+       flt="filter"
 else
-       log_flt="all"
+       flt="all"
 fi
 
-echo "run: $v $s $r $frmt $log_flt"
-$exe_dir/perf-run.sh dist/tackler-cli-$v.jar $s $r "$frmt" "$flt"
+echo "run: $v fs $s $r $frmt $flt"
+$exe_dir/perf-run.sh dist/tackler-cli-$v.jar fs $s $r "$frmt" "$filter"
 
 done
 done
 done
 done
 done
+
+
+for v in $versions; do
+for s in $sets; do
+for r in balance; do
+for frmt in txt; do
+
+filter=""
+
+echo "run: $v git $s $r $frmt all"
+$exe_dir/perf-run.sh dist/tackler-cli-$v.jar git $s $r "$frmt" "$filter"
+
+done
+done
+done
+done
+
--- perf/bin/perf-run.sh
+++ perf/bin/perf-run.sh
@@ -1,69 +1,70 @@
 #!/bin/bash
 
-exe_path=$1
-trg=$2
-report=$3
-formats="$4"
-filter="$5"
+exe_path="$1"
+storage="$2"
+trg="$3"
+report="$4"
+formats="$5"
+filter="$6"
 
-storage=fs
 
 build="$(java -jar $exe_path --version | sed 's/^Version: //')"
 version="$(echo $build | sed 's/\([^ ]\+\) \[.*/\1/')"
 
-if [ "$version" = "0.4.1" ]; then
-   fs=txn
+if [ "$storage" = "fs" ];then
+	if [ "$version" = "0.4.1" ]; then
+   		fs=txn
+	else
+   		fs=fs
+	fi
+	inputSelector=" --input.$fs.dir data/txns-$trg --input.$fs.glob **.txn "
+
+
+elif [ "$storage" = "git" ]; then
+	inputSelector="--input.git.ref txns-$trg"
 else
-   fs=fs
+	echo "unkonwn storage: $storage"
+	exit 1
 fi
+
 
 
 if [ -n "$filter" ]; then 
        flt="flt"
+       fltOpts="--api-filter-def ${filter}"
 else
        flt="all"
+       fltOpts=
 fi
+
 
 report_file=results/hwXX/$version-perf-$flt-$storage-$trg-$report-"$(echo $formats | tr ' ' '_')".txt
 
 (
 echo "exe: $exe_path"
 echo "build: $build"
+echo "storage: $storage"
 echo "set: $trg"
 echo "version: $version"
-echo "storage: $storage"
 echo "report: $report"
 echo "formats: $formats"
 echo "filter: $filter"
 echo ""
 
-# --input.git.ref $trg \
 
 for i in 1 2 3 4 5; do 
 
-if [ -n "$filter" ]; then 
 	/usr/bin/time -f "\nreal\t%es\nuser\t%Us\nsys\t%Ss\nmem\t%Mk (max)\ncpu\t%P" \
 	java -Xmx8G -Xms8G -jar "$exe_path" \
 	--cfg perf-$storage.conf \
-	--input.$fs.glob "**.txn" \
-	--input.$fs.dir data/perf-$trg/ \
-	--output out/perf-$storage-$trg-filter \
+	$inputSelector \
+	--output out/perf-$storage-$trg-$flt \
 	--reporting.console false \
 	--reporting.formats $formats \
 	--reporting.reports $report \
-	--api-filter-def "${filter}"
-else
-	/usr/bin/time -f "\nreal\t%es\nuser\t%Us\nsys\t%Ss\nmem\t%Mk (max)\ncpu\t%P" \
-	java -Xmx8G -Xms8G -jar "$exe_path" \
-	--cfg perf-$storage.conf \
-	--input.$fs.glob "**.txn" \
-	--input.$fs.dir data/perf-$trg/ \
-	--output out/perf-$storage-$trg-all \
-	--reporting.console false \
-	--reporting.formats $formats \
-	--reporting.reports $report 
-fi
+	$fltOpts
 	echo
+
 done
 ) > "$report_file"  2>&1
 
