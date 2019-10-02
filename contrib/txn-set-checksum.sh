e4512ddce2525b7f41f4cefc9c4da8a62a002157
35V LG84 <35vlg84-x4e6b92@e257.fi>
2019-10-02 18:47:46 +0300

    contrib: use format v2 with txn-set-checksum.sh
    Change example script to use new journal format v2
    while parsing Txn UUIDs.
    
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- contrib/txn-set-checksum.sh
+++ contrib/txn-set-checksum.sh
@@ -20,7 +20,12 @@
 #
 # Tackler example script: verify txn set checksum
 #
-
+#   1. Collect all Transcation UUIDs
+#   2. Normalize UUIDs to lovercase form
+#   3. Sort UUIDs
+#   4. Compute SHA-256
+#   5. Print out plain hash
+#
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <txns dir>"
     exit 1
@@ -30,10 +35,11 @@ txns_dir="$1"
 
 (
     find "$txns_dir"  -type f -name '*.txn' |\
-    xargs -n10 grep -h ';:uuid:'
+    xargs -n10 grep -E -h '^[[:space:]]+#[[:space:]]+uuid:[[:space:]]+'
 ) |\
-    sed -E 's/[[:space:]]+;:uuid:[[:space:]]+([a-fA-F0-9-]+)[[:space:]]*/\1/' |\
+    sed -E 's/^[[:space:]]+#[[:space:]]+uuid:[[:space:]]+([a-fA-F0-9-]+)[[:space:]]*/\1/' |\
     tr 'A-F' 'a-f' |\
     sort |\
     sha256sum |\
     sed -E 's/([a-f0-9]+) +-/\1/'
+
