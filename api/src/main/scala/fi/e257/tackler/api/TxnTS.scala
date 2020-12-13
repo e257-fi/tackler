488a89991206df44e5c09d02869eccb6a2900c22
35V LG84 <35vlg84-x4e6b92@e257.fi>
2020-12-13 21:03:54 +0200

    tep-1012: new TxnTS api methods
    * TxnTS
    ** zoned (`tzXXX`) methods
    ** local (`localXXX`) methods
    ** tests
    
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- api/src/main/scala/fi/e257/tackler/api/TxnTS.scala
+++ api/src/main/scala/fi/e257/tackler/api/TxnTS.scala
@@ -19,31 +19,84 @@ package fi.e257.tackler.api
 import java.time.{ZoneId, ZonedDateTime}
 import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder, SignStyle}
 import java.time.temporal.ChronoField.DAY_OF_WEEK
-import java.time.temporal.IsoFields
+import java.time.temporal.{ChronoField, IsoFields}
 
 object TxnTS {
 
-  private val frmtISOWeek = new DateTimeFormatterBuilder()
-    .appendValue(IsoFields.WEEK_BASED_YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
-    .appendLiteral("-W")
-    .appendValue(IsoFields.WEEK_OF_WEEK_BASED_YEAR, 2)
-    .appendOffset("+HH:MM", "Z")
-    .toFormatter
+  private def zonedFormatter(formatter: DateTimeFormatterBuilder, zoneSpace: String) = {
+    formatter
+      .appendLiteral(zoneSpace)
+      .appendOffset("+HH:MM", "Z")
+      .toFormatter
+  }
+
+  private def secondsPartial(): DateTimeFormatterBuilder = {
+    new DateTimeFormatterBuilder()
+      .appendValue(ChronoField.YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+      .appendLiteral('-').appendValue(ChronoField.MONTH_OF_YEAR, 2)
+      .appendLiteral('-').appendValue(ChronoField.DAY_OF_MONTH, 2)
+      .appendLiteral(" ")
+      .appendValue(ChronoField.HOUR_OF_DAY, 2)
+      .appendLiteral(':').appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+      .appendLiteral(':').appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+  }
+
+  private def localSecondsFormatter() = {
+    secondsPartial().toFormatter
+  }
+
+  private def fullPartial() = {
+    secondsPartial()
+      .optionalStart().appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+  }
+
+  private def localFullFormatter() = {
+    fullPartial().toFormatter()
+  }
+
+  private val frmtZonedSeconds = zonedFormatter(secondsPartial(), " ")
+  private val frmtLocalSeconds = localSecondsFormatter()
+
+  private val frmtZonedFullTs = zonedFormatter(fullPartial(), " ")
+  private val frmtLocalFullTs = localFullFormatter()
+
+
+  private def weekPartial() = {
+    new DateTimeFormatterBuilder()
+      .appendValue(IsoFields.WEEK_BASED_YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+      .appendLiteral("-W")
+      .appendValue(IsoFields.WEEK_OF_WEEK_BASED_YEAR, 2)
+  }
 
   // no zoneId as with ISO_WEEK_DATE
   // no localized day number as with 'e' (e.g. en_US => sunday == 1)
-  private val frmtISOWeekDate = new DateTimeFormatterBuilder()
-    .appendValue(IsoFields.WEEK_BASED_YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
-    .appendLiteral("-W")
-    .appendValue(IsoFields.WEEK_OF_WEEK_BASED_YEAR, 2)
-    .appendLiteral('-')
-    .appendValue(DAY_OF_WEEK, 1)
-    .appendOffset("+HH:MM", "Z")
-    .toFormatter
+  private def weekDatePartial() = {
+    weekPartial()
+      .appendLiteral('-')
+      .appendValue(DAY_OF_WEEK, 1)
+  }
+
+  private def localWeekFormatter() = {
+    weekPartial().toFormatter
+  }
+
+  private def localWeekDateFormatter() = {
+    weekDatePartial().toFormatter
+  }
+
+  private val frmtISOWeek = zonedFormatter(weekPartial(), "")
+  private val frmtISOWeekDate = zonedFormatter(weekDatePartial(), "")
+
+  private val frmtTzWeek  = zonedFormatter(weekPartial(), " ")
+  private val frmtTzWeekDate  = zonedFormatter(weekDatePartial(), " ")
+
+  private val frmtLocalWeek = localWeekFormatter()
+  private val frmtLocalWeekDate = localWeekDateFormatter()
 
   /**
    * ISO-8601 Timestamp with offset.
    *
+   * @param ts timestamp
    * @return ISO-8601 date-time: 2016-12-17T12:31:12+03:00
    */
   def isoZonedTS(ts: ZonedDateTime): String = {
@@ -53,6 +106,7 @@ object TxnTS {
   /**
    * ISO-8601 date with offset.
    *
+   * @param ts timestamp
    * @return ISO-8601 date: 2016-12-17T12:31:12+03:00 => 2016-12-17+03:00
    */
   def isoDate(ts: ZonedDateTime): String = {
@@ -82,6 +136,7 @@ object TxnTS {
   /**
    * ISO-8601 Week with offset.
    *
+   * @param ts timestamp
    * @return ISO-8601 week (without date): 2010-01-01 => 2009-W53+03:00
    */
   def isoWeek(ts: ZonedDateTime): String = {
@@ -91,11 +146,160 @@ object TxnTS {
   /**
    * ISO-8601 Week date with offset.
    *
+   * @param ts timestamp
    * @return ISO-8601 week date: 2010-01-01 => 2009-W53-5+03:00
    */
   def isoWeekDate(ts: ZonedDateTime): String = {
     ts.format(frmtISOWeekDate)
   }
+
+  /**
+   * Zoned timestamp (seconds)
+   *
+   * @param ts timestamp
+   * @return date time (s) with offset: 2016-12-17 12:31:12 +03:00
+   */
+  def tzSeconds(ts: ZonedDateTime): String = {
+    ts.format(frmtZonedSeconds)
+  }
+
+  /**
+   * Zoned timestamp (nanoseconds)
+   *
+   * @param ts timestamp
+   * @return date time (ns) with offset: 2016-12-17 12:31:12.123456789 +03:00
+   */
+  def tzFull(ts: ZonedDateTime): String = {
+    ts.format(frmtZonedFullTs)
+  }
+
+  /**
+   * Zoned timestamp (date)
+   *
+   * @param ts timestamp
+   * @return  date with offset: 2016-12-17 +03:00
+   */
+  def tzDate(ts: ZonedDateTime): String = {
+    ts.format(DateTimeFormatter.ofPattern("yyyy-MM-dd XXX"))
+  }
+
+  /**
+   * Zoned timestamp (month)
+   *
+   * @param ts timestamp
+   * @return year-month with offset: 2016-12 +03:00
+   */
+  def tzMonth(ts: ZonedDateTime): String = {
+    ts.format(DateTimeFormatter.ofPattern("yyyy'-'MM XXX"))
+  }
+
+  /**
+   * Zoned timestamp (year)
+   *
+   * @param ts timestamp
+   * @return year with offset: 2016 +03:00
+   */
+  def tzYear(ts: ZonedDateTime): String = {
+    ts.format(DateTimeFormatter.ofPattern("yyyy XXX"))
+  }
+
+  /**
+   * Zoned timestamp (year-week with ISO-8601 rules)
+   *
+   * @param ts timestamp
+   * @return year-week with offset: 2009-W53 +03:00
+   */
+  def tzWeek(ts: ZonedDateTime): String = {
+    ts.format(frmtTzWeek)
+  }
+
+  /**
+   * Zoned timestamp (year-week-day with ISO-8601 rules)
+   *
+   * @param ts timestamp
+   * @return year-week-date and offset: 2009-W53-5 +03:00
+   */
+  def tzWeekDate(ts: ZonedDateTime): String = {
+    ts.format(frmtTzWeekDate)
+  }
+
+  /**
+   * Local timestamp (seconds)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return local ts (seconds): 2016-12-17 12:31:12
+   */
+  def localSeconds(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(frmtLocalSeconds)
+  }
+
+  /**
+   * Local timestamp (nanoseconds)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return local ts (nanoseconds): 2016-12-17 12:31:12.123456789
+   */
+  def localFull(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(frmtLocalFullTs)
+  }
+
+  /**
+   * Local timestamp (date)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return local ts (date): 2016-12-17
+   */
+  def localDate(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+  }
+
+  /**
+   * Local timestamp (month)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return local ts (months): 2016-12
+   */
+  def localMonth(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(DateTimeFormatter.ofPattern("yyyy'-'MM"))
+  }
+
+  /**
+   * Local timestamp (year)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return Local ts (year): 2016
+   */
+  def localYear(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(DateTimeFormatter.ofPattern("yyyy"))
+  }
+
+  /**
+   *  Local timestamp (year-week with ISO-8601 rules)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return local year-week: 2009-W53
+   */
+  def localWeek(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(frmtLocalWeek)
+  }
+
+  /**
+   * Local timestamp (week-date with ISO-8601 rules)
+   *
+   * @param ts timestamp
+   * @param localTZ local time zone
+   * @return local week date: 2009-W53-5
+   */
+  def localWeekDate(ts: ZonedDateTime, localTZ: ZoneId): String = {
+    ts.withZoneSameInstant(localTZ).format(frmtLocalWeekDate)
+  }
+
 
   object Shard {
     private def toUTC(ts: ZonedDateTime): ZonedDateTime = ts.withZoneSameInstant(ZoneId.of("UTC"))
--- api/src/test/scala/fi/e257/tackler/api/TxnTSTest.scala
+++ api/src/test/scala/fi/e257/tackler/api/TxnTSTest.scala
@@ -16,7 +16,7 @@
  */
 package fi.e257.tackler.api
 
-import java.time.ZonedDateTime
+import java.time.{ZoneId, ZonedDateTime}
 
 import org.scalatest.funspec.AnyFunSpec
 
@@ -28,7 +28,7 @@ class TxnTSTest extends AnyFunSpec {
   }
 
   describe("ISO-8601 timestamps utilities") {
-    describe("Format to String") {
+    describe("Format to String: ISO 8601") {
 
       it("isoDate") {
         assert(TxnTS.isoDate(txt2ts("2010-01-02T00:00:00Z")) === "2010-01-02Z")
@@ -78,6 +78,157 @@ class TxnTSTest extends AnyFunSpec {
         assert(TxnTS.isoWeekDate(txt2ts("2017-01-02T00:00:00-02:00")) === "2017-W01-1-02:00")
       }
     }
+
+    describe("Format to String: Zoned") {
+
+      it("tzDate") {
+        assert(TxnTS.tzDate(txt2ts("2010-01-02T00:00:00Z")) === "2010-01-02 Z")
+        assert(TxnTS.tzDate(txt2ts("2010-01-02T00:00:00+02:00")) === "2010-01-02 +02:00")
+        assert(TxnTS.tzDate(txt2ts("2010-01-02T00:00:00-02:00")) === "2010-01-02 -02:00")
+        assert(TxnTS.tzDate(txt2ts("2020-12-31T23:59:58Z")) === "2020-12-31 Z")
+      }
+
+      it("tzMonth") {
+        assert(TxnTS.tzMonth(txt2ts("2010-01-02T00:00:00Z")) === "2010-01 Z")
+        assert(TxnTS.tzMonth(txt2ts("2010-01-02T00:00:00+02:00")) === "2010-01 +02:00")
+        assert(TxnTS.tzMonth(txt2ts("2010-01-02T00:00:00-02:00")) === "2010-01 -02:00")
+        assert(TxnTS.tzMonth(txt2ts("2020-12-31T23:59:58Z")) === "2020-12 Z")
+      }
+
+      it("tzYear") {
+        assert(TxnTS.tzYear(txt2ts("2010-01-02T00:00:00Z")) === "2010 Z")
+        assert(TxnTS.tzYear(txt2ts("2010-01-02T00:00:00+02:00")) === "2010 +02:00")
+        assert(TxnTS.tzYear(txt2ts("2010-01-02T00:00:00-02:00")) === "2010 -02:00")
+        assert(TxnTS.tzYear(txt2ts("2020-12-31T23:59:58-02:00")) === "2020 -02:00")
+      }
+
+      it("tzSeconds") {
+        assert(TxnTS.tzSeconds(txt2ts("2010-01-01T00:00:00+16:00"))        === "2010-01-01 00:00:00 +16:00")
+        assert(TxnTS.tzSeconds(txt2ts("2010-01-01T01:02:03.456+16:00"))    === "2010-01-01 01:02:03 +16:00")
+        assert(TxnTS.tzSeconds(txt2ts("2010-01-01T01:02:03.456789+16:00")) === "2010-01-01 01:02:03 +16:00")
+        assert(TxnTS.tzSeconds(txt2ts("2010-01-01T01:02:03.700-16:00"))    === "2010-01-01 01:02:03 -16:00")
+
+        assert(TxnTS.tzSeconds(txt2ts("2010-01-01T00:00:00+00:00"))        === "2010-01-01 00:00:00 Z")
+
+        assert(TxnTS.tzSeconds(txt2ts("2020-12-31T23:58:59+00:00"))        === "2020-12-31 23:58:59 Z")
+      }
+
+      it("tzFull") {
+        assert(TxnTS.tzFull(txt2ts("2010-01-01T00:00:00+16:00"))            === "2010-01-01 00:00:00 +16:00")
+        assert(TxnTS.tzFull(txt2ts("2010-01-01T01:02:03.456+16:00"))        === "2010-01-01 01:02:03.456 +16:00")
+        assert(TxnTS.tzFull(txt2ts("2010-01-01T01:02:03.456789+16:00"))     === "2010-01-01 01:02:03.456789 +16:00")
+        assert(TxnTS.tzFull(txt2ts("2010-01-02T11:12:13.123456789+16:00"))  === "2010-01-02 11:12:13.123456789 +16:00")
+        assert(TxnTS.tzFull(txt2ts("2010-01-01T01:02:03.700-16:00"))        === "2010-01-01 01:02:03.7 -16:00")
+        assert(TxnTS.tzFull(txt2ts("2010-01-01T00:00:00+00:00"))            === "2010-01-01 00:00:00 Z")
+
+        assert(TxnTS.tzFull(txt2ts("2020-12-31T23:58:59.123456789+00:00"))  === "2020-12-31 23:58:59.123456789 Z")
+      }
+
+      it("tzWeek") {
+        assert(TxnTS.tzWeek(txt2ts("2010-01-03T00:00:00+00:00")) === "2009-W53 Z")
+        assert(TxnTS.tzWeek(txt2ts("2010-01-04T00:00:00+00:00")) === "2010-W01 Z")
+        assert(TxnTS.tzWeek(txt2ts("2017-01-01T00:00:00+00:00")) === "2016-W52 Z")
+        assert(TxnTS.tzWeek(txt2ts("2017-01-02T00:00:00+00:00")) === "2017-W01 Z")
+
+        assert(TxnTS.tzWeek(txt2ts("2017-01-02T00:00:00+02:00")) === "2017-W01 +02:00")
+        assert(TxnTS.tzWeek(txt2ts("2017-01-02T00:00:00-02:00")) === "2017-W01 -02:00")
+
+        assert(TxnTS.tzWeek(txt2ts("2020-12-31T00:00:00-02:00")) === "2020-W53 -02:00")
+      }
+
+      it("tzWeekDate") {
+        assert(TxnTS.tzWeekDate(txt2ts("2010-01-03T00:00:00+00:00")) === "2009-W53-7 Z")
+        assert(TxnTS.tzWeekDate(txt2ts("2010-01-04T00:00:00+00:00")) === "2010-W01-1 Z")
+        assert(TxnTS.tzWeekDate(txt2ts("2017-01-01T00:00:00+00:00")) === "2016-W52-7 Z")
+        assert(TxnTS.tzWeekDate(txt2ts("2017-01-02T00:00:00+00:00")) === "2017-W01-1 Z")
+        assert(TxnTS.tzWeekDate(txt2ts("2017-01-02T00:00:00Z")) === "2017-W01-1 Z")
+
+        assert(TxnTS.tzWeekDate(txt2ts("2017-01-02T00:00:00+02:00")) === "2017-W01-1 +02:00")
+        assert(TxnTS.tzWeekDate(txt2ts("2017-01-02T00:00:00-02:00")) === "2017-W01-1 -02:00")
+
+        assert(TxnTS.tzWeekDate(txt2ts("2020-12-31T00:00:00-02:00")) === "2020-W53-4 -02:00")
+      }
+    }
+
+    describe("Format to String: localTZ") {
+
+      val localTZ = ZoneId.of("Z")
+
+      it("localDate") {
+        assert(TxnTS.localDate(txt2ts("2010-01-02T00:00:00Z"), localTZ) === "2010-01-02")
+        assert(TxnTS.localDate(txt2ts("2010-01-02T00:00:00+02:00"), localTZ) === "2010-01-01")
+        assert(TxnTS.localDate(txt2ts("2010-01-02T00:00:00-02:00"), localTZ) === "2010-01-02")
+        assert(TxnTS.localDate(txt2ts("2020-12-31T23:58:59Z"), localTZ) === "2020-12-31")
+      }
+
+      it("localMonth") {
+        assert(TxnTS.localMonth(txt2ts("2010-02-01T00:00:00Z"), localTZ) === "2010-02")
+        assert(TxnTS.localMonth(txt2ts("2010-02-01T00:00:00+02:00"), localTZ) === "2010-01")
+        assert(TxnTS.localMonth(txt2ts("2010-02-01T00:00:00-02:00"), localTZ) === "2010-02")
+        assert(TxnTS.localMonth(txt2ts("2020-12-31T23:58:59Z"), localTZ) === "2020-12")
+      }
+
+      it("localYear") {
+        assert(TxnTS.localYear(txt2ts("2010-01-02T00:00:00Z"), localTZ) === "2010")
+        assert(TxnTS.localYear(txt2ts("2010-01-01T00:00:00+02:00"), localTZ) === "2009")
+        assert(TxnTS.localYear(txt2ts("2010-01-01T00:00:00-02:00"), localTZ) === "2010")
+        assert(TxnTS.localYear(txt2ts("2020-12-31T23:58:59Z"), localTZ) === "2020")
+      }
+
+      it("localSeconds") {
+        assert(TxnTS.localSeconds(txt2ts("2010-01-02T00:00:00+16:00"), localTZ)        === "2010-01-01 08:00:00")
+        assert(TxnTS.localSeconds(txt2ts("2010-01-02T01:02:03.456+16:00"), localTZ)    === "2010-01-01 09:02:03")
+        assert(TxnTS.localSeconds(txt2ts("2010-01-02T01:02:03.456789+16:00"), localTZ) === "2010-01-01 09:02:03")
+        assert(TxnTS.localSeconds(txt2ts("2010-01-02T01:02:03.700-16:00"), localTZ)    === "2010-01-02 17:02:03")
+
+        assert(TxnTS.localSeconds(txt2ts("2010-01-01T00:00:00+00:00"), localTZ)        === "2010-01-01 00:00:00")
+        assert(TxnTS.localSeconds(txt2ts("2020-12-31T23:58:59.123456789+00:00"), localTZ)  === "2020-12-31 23:58:59")
+      }
+
+      it("localFull") {
+        assert(TxnTS.localFull(txt2ts("2010-01-02T00:00:00+16:00"), localTZ)        === "2010-01-01 08:00:00")
+        assert(TxnTS.localFull(txt2ts("2010-01-02T01:02:03.456+16:00"), localTZ)    === "2010-01-01 09:02:03.456")
+        assert(TxnTS.localFull(txt2ts("2010-01-02T01:02:03.456789+16:00"), localTZ) === "2010-01-01 09:02:03.456789")
+        assert(TxnTS.localFull(txt2ts("2010-01-02T01:02:03.700-16:00"), localTZ)    === "2010-01-02 17:02:03.7")
+
+        assert(TxnTS.localFull(txt2ts("2010-01-01T13:35:56+00:00"), localTZ)        === "2010-01-01 13:35:56")
+        assert(TxnTS.localFull(txt2ts("2020-12-31T23:58:59.123456789+00:00"), localTZ)  === "2020-12-31 23:58:59.123456789")
+      }
+
+      it("localWeek") {
+        assert(TxnTS.localWeek(txt2ts("2010-01-03T00:00:00+00:00"), localTZ) === "2009-W53")
+
+        assert(TxnTS.localWeek(txt2ts("2010-01-04T00:00:00+02:00"), localTZ) === "2009-W53")
+        assert(TxnTS.localWeek(txt2ts("2010-01-04T02:00:00+02:00"), localTZ) === "2010-W01")
+
+        assert(TxnTS.localWeek(txt2ts("2017-01-01T00:00:00+00:00"), localTZ) === "2016-W52")
+
+        assert(TxnTS.localWeek(txt2ts("2017-01-02T00:00:00+02:00"), localTZ) === "2016-W52")
+        assert(TxnTS.localWeek(txt2ts("2017-01-02T02:00:00+02:00"), localTZ) === "2017-W01")
+
+        assert(TxnTS.localWeek(txt2ts("2017-01-02T02:00:00+02:00"), localTZ) === "2017-W01")
+        assert(TxnTS.localWeek(txt2ts("2017-01-01T22:00:00-02:00"), localTZ) === "2017-W01")
+
+        assert(TxnTS.localWeek(txt2ts("2020-12-31T00:00:00-02:00"), localTZ) === "2020-W53")
+      }
+
+      it("localWeekDate") {
+        assert(TxnTS.localWeekDate(txt2ts("2010-01-03T00:00:00+00:00"), localTZ) === "2009-W53-7")
+        assert(TxnTS.localWeekDate(txt2ts("2010-01-04T00:00:00+00:00"), localTZ) === "2010-W01-1")
+
+        assert(TxnTS.localWeekDate(txt2ts("2010-01-04T00:00:00+02:00"), localTZ) === "2009-W53-7")
+        assert(TxnTS.localWeekDate(txt2ts("2010-01-04T02:00:00+02:00"), localTZ) === "2010-W01-1")
+
+        assert(TxnTS.localWeekDate(txt2ts("2017-01-01T00:00:00+00:00"), localTZ) === "2016-W52-7")
+        assert(TxnTS.localWeekDate(txt2ts("2017-01-02T00:00:00+00:00"), localTZ) === "2017-W01-1")
+        assert(TxnTS.localWeekDate(txt2ts("2017-01-02T00:00:00Z"), localTZ) === "2017-W01-1")
+
+        assert(TxnTS.localWeekDate(txt2ts("2017-01-02T02:00:00+02:00"), localTZ) === "2017-W01-1")
+        assert(TxnTS.localWeekDate(txt2ts("2017-01-01T22:00:00-02:00"), localTZ) === "2017-W01-1")
+        assert(TxnTS.localWeekDate(txt2ts("2020-12-31T00:00:00-02:00"), localTZ) === "2020-W53-4")
+      }
+    }
+
   }
 
 
