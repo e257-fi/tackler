e257-fi/tackler

commit 184bdd623d3376ad17e2f293625441fd34e03f12

Update build and deps, fix cli/assembly target
build.sbt: cli/assembly: fix merge strategy, it broke with new logback

* Dependencies
** Antlr4: 4.11.1
** cats: 2.9.0
** circe: 0.14.3
** config: 1.4.2
** scala-parallel-collections: 1.0.4
** jgit: 6.4.0.202211300538-r
** logback: 1.4.5
** slf4j: 2.0.6
* Build
** sbt: 1.8.0
** sbt-assembly: 2.1.0
** sbt-buildinfo: 1.11.0
** sbt-scalajs: 1.12.1
** sbt-crossproject: 1.2.0
** sbt-scalajs-crossproject: 1.2.0
** scala-2.12: 2.12.17
** scala-2.13: 2.13.10
** scala-collection-compat: dropped
* Tests
** sbt-scoverage: 2.0.6
** sbt-wartremover: 3.0.7

Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

build.sbt
@@ -19,7 +19,7 @@ import Dependencies._
 
 import sbtcrossproject.{crossProject, CrossType}
 
-lazy val scala_13 = "2.13.7"
+lazy val scala_13 = "2.13.10"
 
 ThisBuild / organization := "fi.e257"
 ThisBuild / version := "0.36.0-SNAPSHOT"
@@ -130,7 +130,7 @@ lazy val core = (project in file("core")).
   settings(
     name := "tackler-core",
     fork := true,
-    Antlr4 / antlr4Version := "4.9.3",
+    Antlr4 / antlr4Version := "4.11.1",
     Antlr4 / antlr4GenListener := false,
     Antlr4 / antlr4GenVisitor := false,
     Antlr4 / antlr4TreatWarningsAsErrors := true,
@@ -173,17 +173,10 @@ lazy val cli = (project in file("cli")).
     assembly / test := {},
     assembly / assemblyJarName := "tackler-cli" + "-" + version.value + ".jar",
     assembly / assemblyMergeStrategy := {
-      // Scala-collection-compat vs. scala 2.12.13: both have support for nowarn,
-      // see: https://github.com/scala/scala-collection-compat/issues/426
-      case PathList("scala", "annotation", "nowarn.class" | "nowarn$.class") =>
-          MergeStrategy.first
       // Fix (e.g. discard) module-info.class with JDK 8 vs. JDK 11,
       // it's not needed, this is an app, not lib
-      // Bouncycastle files are living nowdays under META-INF/versions/9, hence endsWith
-      case x if x.endsWith("/module-info.class") => MergeStrategy.discard
-      case x =>
-        val oldStrategy = (assembly / assemblyMergeStrategy).value
-        oldStrategy(x)
+      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+      case x => MergeStrategy.first
     },
     gitCommitId := git.gitHeadCommit.value.getOrElse("Not available"),
     gitLocalChanges := git.gitUncommittedChanges.value,

project/build.sbt
@@ -17,8 +17,8 @@
 
 // build-build deps (TestSetup etc.)
 
-scalaVersion := "2.12.15"
+scalaVersion := "2.12.17"
 
 libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.9.1"
-libraryDependencies += "com.typesafe" % "config" % "1.4.1"
+libraryDependencies += "com.typesafe" % "config" % "1.4.2"
 

cli/src/test/scala/fi/e257/tackler/cli/BuildInfoTest.scala
@@ -31,7 +31,7 @@ class BuildInfoTest extends AnyFlatSpec {
   }
 
   it should "sbtVersion" in {
-    assert(BuildInfo.sbtVersion.startsWith("1.6"))
+    assert(BuildInfo.sbtVersion.startsWith("1.8"))
   }
 
   it should "toString" in {

project/Dependencies.scala
@@ -22,17 +22,17 @@ object Dependencies {
    * Versions
    */
   val betterFilesVersion = "3.9.1"
-  val catsVersion = "2.7.0"
-  val circeVersion = "0.14.1"
+  val catsVersion = "2.9.0"
+  val circeVersion = "0.14.3"
   val circeOpticsVersion = "0.14.1"
-  val scalaParCollectionVersion = "1.0.0"
-  val scalatestVersion = "3.2.10"
+  val scalaParCollectionVersion = "1.0.4"
+  val scalatestVersion = "3.2.14"
   val scallopVersion = "4.1.0"
-  val configVersion = "1.4.1"
+  val configVersion = "1.4.2"
   val dirsuiteVersion = "0.31.0"
-  val jgitVersion = "6.0.0.202111291000-r"
-  val logbackVersion = "1.2.10"
-  val slf4jVersion = "1.7.32"
+  val jgitVersion = "6.4.0.202211300538-r"
+  val logbackVersion = "1.4.5"
+  val slf4jVersion = "2.0.6"
 
   /*
    * Libraries

project/build.properties
@@ -1,1 +1,1 @@
-sbt.version=1.6.1
+sbt.version=1.8.0

project/plugins.sbt
@@ -1,20 +1,20 @@
 // code generators
-addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.10.0")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
 addSbtPlugin("com.simplytyped" % "sbt-antlr4" % "0.8.3")
 
 // ScalaJS
-addSbtPlugin("org.scala-js"       % "sbt-scalajs"              % "1.8.0")
-addSbtPlugin("org.portable-scala" % "sbt-crossproject"         % "1.1.0")
-addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.1.0")
+addSbtPlugin("org.scala-js"       % "sbt-scalajs"              % "1.12.0")
+addSbtPlugin("org.portable-scala" % "sbt-crossproject"         % "1.2.0")
+addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.2.0")
 
 // build & release
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "1.1.0")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.1.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "1.0.2")
 
 // QA tools
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.2")
-addSbtPlugin("org.wartremover" % "sbt-wartremover" % "2.4.16")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.6")
+addSbtPlugin("org.wartremover" % "sbt-wartremover" % "3.0.7")
 
 // Publishing
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.10")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.15")
 

commit b5d1c49fc6d1c9f6752a388f6a1de458e4076840

build: Fix broken logging for tacler-cli.jar (cli/assembly target)
This reverts partially 184bdd623d3376ad17e2f293625441fd34e03f12
which broke assembly target's merge strategy.

Restore old strategy for 'module-info.class' and define new,
additional strategy for cases when 'module-info.class'
is located at root (logback-* jars).

Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

build.sbt
@@ -193,12 +193,17 @@ lazy val cli = (project in file("cli")).
       Tests.Setup(() => TacklerTests.setup("tests", log))
     },
     assembly / test := {},
+    assembly / mainClass := Some("fi.e257.tackler.cli.TacklerCli"),
     assembly / assemblyJarName := "tackler-cli" + "-" + version.value + ".jar",
     assembly / assemblyMergeStrategy := {
       // Fix (e.g. discard) module-info.class with JDK 8 vs. JDK 11,
       // it's not needed, this is an app, not lib
-      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-      case x => MergeStrategy.first
+      case "module-info.class" => MergeStrategy.discard
+      // Discard module-info also from multi java jars (e.g. META-INF/versions/9/module-info.class)
+      case x if x.endsWith("/module-info.class") => MergeStrategy.discard
+      case x =>
+        val oldStrategy = (assembly / assemblyMergeStrategy).value
+        oldStrategy(x)
     },
     gitCommitId := git.gitHeadCommit.value.getOrElse("Not available"),
     gitLocalChanges := git.gitUncommittedChanges.value,